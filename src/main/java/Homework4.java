bd841d25530daa141c38b81296aebab8d2c3aa1f
Elnur <eeliyev@yahoo.com>
2019-10-21 23:22:49 +0400

    --initialClassPet

---
+++ src/main/java/Homework4.java
@@ -0,0 +1,7 @@
+public class Homework4 {
+    public static void main(String[] args) {
+        Pet p1= new Pet("cirt","bobik",10,20);
+
+        System.out.println(p1.age);
+    }
+}
---
+++ src/main/java/Pet.java
@@ -0,0 +1,24 @@
+public class Pet {
+    String species;
+    String nickname;
+    int age;
+    int trickLevel;
+    //String habits[];
+
+    Pet (String species, String nickname, int age, int trickLevel)
+    {
+        this.species=species;
+        this.nickname=nickname;
+        this.age=age;
+        this.trickLevel=trickLevel;
+        //this.habits=habits;
+
+
+
+    }
+
+
+}
+
+
+
